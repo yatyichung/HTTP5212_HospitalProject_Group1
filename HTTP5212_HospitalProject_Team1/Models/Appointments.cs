f85a7b31ceb0c93a4ae2f711e4801e8a17d6261c
yatyichung <chung_yatyi@yahoo.com.hk>
2022-07-06 17:17:14 -0400

    addes 9 classes in Model folder - Sunny

---
+++ HTTP5212_HospitalProject_Team1/Models/Appointments.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HTTP5212_HospitalProject_Team1.Models
+{
+    public class Appointments
+    {
+    }
+}
