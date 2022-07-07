127b8e2e7a3dfa77a78209ec974e85a586c0f7ad
Tara Lai <taralai.developer@gmail.com>
2022-07-06 22:14:04 -0700

    {action} added

--- HTTP5212_HospitalProject_Team1/App_Start/WebApiConfig.cs
+++ HTTP5212_HospitalProject_Team1/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace HTTP5212_HospitalProject_Team1
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
