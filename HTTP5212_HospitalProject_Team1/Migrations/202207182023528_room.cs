6da98fdf9aec2cf394c17c511fc2eeebe0cd72f8
Tara Lai <taralai.developer@gmail.com>
2022-07-18 13:33:26 -0700

    room database added

---
+++ HTTP5212_HospitalProject_Team1/Migrations/202207182023528_room.cs
@@ -0,0 +1,28 @@
+﻿namespace HTTP5212_HospitalProject_Team1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class room : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Rooms",
+                c => new
+                    {
+                        RoomId = c.Int(nullable: false, identity: true),
+                        RoomType = c.String(),
+                        BlockFloor = c.Int(nullable: false),
+                        Availability = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.RoomId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Rooms");
+        }
+    }
+}
--- HTTP5212_HospitalProject_Team1/Models/Room.cs
+++ HTTP5212_HospitalProject_Team1/Models/Room.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace HTTP5212_HospitalProject_Team1.Models
 {
     public class Room
     {
+        [Key]
+        public int RoomId { get; set; }
+
+        public string RoomType { get; set; }
+
+        public int BlockFloor { get; set; }
+
+        public bool Availability { get; set; }
     }
 }
