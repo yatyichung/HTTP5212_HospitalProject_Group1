0a12fccc88cfa54be869b784bd3a46eaefc4a383
yatyichung <chung_yatyi@yahoo.com.hk>
2022-07-18 15:17:37 -0400

    Enable Migrations - Sunny

---
+++ HTTP5212_HospitalProject_Team1/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace HTTP5212_HospitalProject_Team1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<HTTP5212_HospitalProject_Team1.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "HTTP5212_HospitalProject_Team1.Models.ApplicationDbContext";
+        }
+
+        protected override void Seed(HTTP5212_HospitalProject_Team1.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
