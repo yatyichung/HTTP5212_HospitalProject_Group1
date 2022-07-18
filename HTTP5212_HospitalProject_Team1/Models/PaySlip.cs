75c79a1f831c0c42a1a70262845bcad90158f060
yatyichung <chung_yatyi@yahoo.com.hk>
2022-07-18 15:38:23 -0400

    added primary key for PaySlips.cs - Sunny declare data types in Shift.cs - Sunny

--- HTTP5212_HospitalProject_Team1/Models/PaySlip.cs
+++ HTTP5212_HospitalProject_Team1/Models/PaySlip.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace HTTP5212_HospitalProject_Team1.Models
 {
     public class PaySlip
     {
+        [Key]
         public int PaySlipID { get; set; }
 
         public int PaySlipHoursWorked { get; set; }
