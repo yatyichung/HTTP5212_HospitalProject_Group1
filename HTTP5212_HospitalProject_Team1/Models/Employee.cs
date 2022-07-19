9ab6e05a2f53f9f39509c4deeba2967dbbaf7c23
Andrew_Rezk <andrew_rezk@yahoo.com>
2022-07-18 20:38:27 -0400

    added models- Andrew

--- HTTP5212_HospitalProject_Team1/Models/Employee.cs
+++ HTTP5212_HospitalProject_Team1/Models/Employee.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace HTTP5212_HospitalProject_Team1.Models
 {
     public class Employee
     {
+        [Key]
+        public int EmployeeID { get; set; }
+
+        public string EmployeeFirstName { get; set; }
+
+        public string EmployeeLastName { get; set; }
+
+        public string EmployeeRole { get; set; }
+
+        public DateTime EmployeeJoinDate { get; set; }
     }
 }
