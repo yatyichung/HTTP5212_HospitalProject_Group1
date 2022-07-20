9ad722828b315176ea65fe97cd5138a44ae0ca98
bethj <bethjarrettbeth@gmail.com>
2022-07-19 22:41:32 -0400

    added models, tables: Department, Service

---
+++ HTTP5212_HospitalProject_Team1/Controllers/DepartmentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Diagnostics;
+using HTTP5212_HospitalProject_Team1.Models;
+using System.Web.Script.Serialization;
+
+namespace HTTP5212_HospitalProject_Team1.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        // GET: Department
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- HTTP5212_HospitalProject_Team1/Models/Department.cs
+++ HTTP5212_HospitalProject_Team1/Models/Department.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace HTTP5212_HospitalProject_Team1.Models
 {
     public class Department
     {
+        [Key]
+        public int dept_id { get; set; }
+        public string dept_name { get; set; }
+        public string dept_desc { get; set; }
     }
 }
